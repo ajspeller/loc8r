b3973cc52f1607d15e57a1e9232d7fcca40050e2
AJ Speller <ajspeller@gmail.com>
2018-09-06 18:59:35 -0400

    Adding the view templates

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,23 @@
+const homeList = (req, res) => {
+  res.render('locations-list', {
+    title: 'Home'
+  });
+}
+
+const locationInfo = (req, res) => {
+  res.render('location-info', {
+    title: 'Location Info'
+  });
+}
+
+const addReview = (req, res) => {
+  res.render('location-review-form', {
+    title: 'Add Review'
+  });
+}
+
+module.exports = {
+  homeList,
+  locationInfo,
+  addReview,
+};
